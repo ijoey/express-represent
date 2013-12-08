5cd04aff9757117ad34c55f0e6192c8132359634
Joey Guerra <joey@joeyguerra.com>
2013-12-08 17:40:13 -0600

    refactoring represent and overriding express send

--- app.js
+++ app.js
@@ -1,53 +1,99 @@
 var port = process.env.PORT;
+var config = {};
 var runAsUser = null;
 var express = require('express');
+var http = require('http');
 var app = express();
-var fs = require('fs');
+var Fs = require('fs');
 var cachedResources = [];
-var resourcesFolder = __dirname + "/resources";
-var Represent = require("./represent");
+var rootPath = __dirname.replace('lib', '');
+var resourcesFolder = rootPath + "/resources";
+var Represent = require("./lib/represent");
+function getEnvironmentConfig(){
+	return require('./lib/env.js');
+}
+function ErrorMessage(error){
+	if(typeof error === Object || error === 500){
+		this.message = "Internal app Error";
+		this.code = 500;
+	}else if(error === 401){
+		this.message = "Unauthorized";
+		this.code = 401;
+	}else if(error === 404){
+		this.message = "Not found";
+		this.code = 404;
+	}
+}
+function requiredConfigsNotSet(){
+	return process.env.THEME === undefined;
+}
 
+/* override express's response send with Represent.*/
+express.response.send = function(result){
+	if(!result.view){
+		this.statusCode = result;
+		this.set('Content-Type', 'text/plain');
+		return this.end(arguments.length > 1 ? arguments[1] : null);
+	}
+	result.resource.user = this.req.user;
+	Represent.execute({
+		next: this.req.next
+		, model: result.model
+		, request: this.req
+		, response: this
+		, resource: result.resource
+		, view: result.view
+	});
+};
 
-// run as:
-// node app.js port:3001 as:joeyguerra
-// as is the user name to run the process as in UNIX. Doesn't really work in Windows.
-
+if(requiredConfigsNotSet()){
+	process.env = getEnvironmentConfig();
+}
+config.cookie = {
+	secret: process.env.COOKIE_SECRET
+	, key: process.env.COOKIE_KEY
+};
+Represent.themeRoot = process.env.THEME;
 process.argv.forEach(function(value, fileName, args){
 	if(/as:/.test(value)) runAsUser = /as\:([a-zA-Z-]+)/.exec(value)[1];
 	if(/port:/.test(value)) port = /port:(\d+)/.exec(value)[1];
 });
-if(!port) port = 3000;
-app.response.represent = function(view, resource, model, next){
-	resource.user = this.req.user;
-	Represent.execute({next: next, response: this, request: this.req, view: view, resource: resource, model: model});
-};
-app.configure(function(){
-	app.use(express.compress());
-	app.use("/public", express.static(Represent.themeRoot));
-	app.set("views", __dirname + "/themes/default/views");
-	app.set("view engine", function(view, options, fn){
-		return fn(view, options);
-	});
-	app.use(express.methodOverride());
-	app.use(express.bodyParser());
-	app.use(express.cookieParser("somelongpassword...."));
-	app.use(express.cookieSession({ secret: 'somelongpassword....', maxAge: 360*5 }));
-	// load resource files.
-	fs.readdirSync(resourcesFolder).forEach(function(file) {
-		var key = file.replace('.js', '');
-		var resource = require(resourcesFolder + "/" + file)(app);
-		cachedResources.push(resource);
-	});
+if(!port) port = 10000;
+process.on('uncaughtException', function(err){
+    console.trace('got an error:', err);
+    //process.exit(1);
 });
-
+process.on('exit', function() {
+  console.log('exited.');
+});
+//app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
+app.use(express.compress());
+console.log(rootPath + '/' + Represent.themeRoot);
+app.use("/public", express.static(rootPath + '/themes/' + Represent.themeRoot));
+app.set("views", rootPath + '/themes/' + Represent.themeRoot + '/views');
+app.set("view engine", function(view, options, fn){
+	return fn(view, options);
+});
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieSession({ key: config.cookie.key, secret: config.cookie.secret}));
+// load resource files.
+Fs.readdirSync(resourcesFolder).forEach(function(file) {
+	var key = file.replace('.js', '');
+	var resource = require(resourcesFolder + "/" + file)(app);
+	cachedResources.push(resource);
+});
 // send errors back to client.
 app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.send(500, 'Something broke!');
+  //console.trace('error:', JSON.stringify(err));
+  var error = new ErrorMessage(err);
+  res.send(error.message);
 });
 app.listen(port, function(){
 	if(runAsUser !== null){
 		process.setgid(runAsUser);
 		process.setuid(runAsUser);
 	}
+	console.log('listening on ', port);
 });
--- resources/index.js
+++ resources/index.js
@@ -1,14 +1,17 @@
-var Resource = require('../resource');
+var Resource = require('../lib/resource');
 module.exports = function index(app){
 	var self = new Resource();
-	self.title = "express-represnet, organize your code in a ReSTful way.";
+	self.title = "express-represent, represent requests via content negotiation.";
 	self.js.push("index");
 	app.get("/(index)?.:format?", function(req, resp, next){
-		resp.represent("index/index", self, {header: "Purpose", copy: "Use a representational paradigm with ExpressJS."}, next);
+		resp.send({resource: self
+			, view: "index/index"
+			, model:{header: "Purpose", copy: "Use a representational paradigm with ExpressJS."}
+		});
 	});
 	app.post("/(index)?.:format?", function(req, resp, next){
 		var model = {header: "Purpose", copy: req.body.copy};
-		resp.represent("index/index", self, model, next);
+		resp.send({resource: self, view: "index/index", model:model});
 	});
 	return self;
 };
